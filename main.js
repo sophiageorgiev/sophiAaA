ffd312efb22424dc1ff42b28380656e34f8e16b9
sophiageorgiev <99654877+sophiageorgiev@users.noreply.github.com>
2022-05-24 19:14:12 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+canvas.class=
+ block_y=1;
+ block_x=1;
+
+block_image_width = 350;
+block_image_height = 430;
+
+var block_image_object= "";
+
+function new_image(get_image)
+{
+	// to upload images
+}
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+keyPressed = e.keyCode;
+console.log(keyPressed);
+
+	if(keyPressed == '?') 
+	{
+		redRanger==82
+	}
+	if(keyPressed == '71')
+	{
+		block_x = 200;
+		greenRanger==71
+	}
+	
+	if(keyPressed == '89')
+	{
+		block_x =350;
+		yellowRanger==89
+	}
+	if(keyPressed == '80')
+	{
+		block_x = 600;
+		pinkRanger==80
+	}
+	if(keyPressed == '66')
+	{
+		block_x = 700;
+        blueRanger==66
+	}
+	
+}
+
